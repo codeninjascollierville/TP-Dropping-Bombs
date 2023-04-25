a6f276f4866b64d3ecb18a55019c98378be0d54b
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-25 18:32:11 -0500

    4/25/23
    On Step 34!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{ 
+        private Spawner spawner;
+        public GameObject title;
+        void Awake()
+        {
+            spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        }
     // Start is called before the first frame update
     void Start()
     {
+        spawner.active = false;
+        title.SetActive(true);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            spawner.active = true;
+            title.SetActive(false);
+        }
     }
 }
